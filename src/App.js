24a8279fceb2bdc13596ae00c30c73730b30dcc0
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2022-09-16 09:45:02 +0300

    routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import "./App.scss";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Catalog from "./pages/Catalog";
+import Details from "./pages/Details";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header></Header>
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/:category" element={<Catalog></Catalog>}></Route>
+          <Route path="/:category/:id" element={<Details></Details>}></Route>
+          <Route
+            path="/:category/search/:keyword"
+            element={<Catalog></Catalog>}
+          ></Route>
+        </Routes>
+        <Footer></Footer>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
